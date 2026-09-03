74561c7445c5a21cbd798493b45369296d72e644
danielHyman123 <danielhyman20@gmail.com>
2026-09-03 01:13:24 -0400

    assem visualized in rviz2

---
+++ launch/display.launch.py
@@ -0,0 +1,32 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('robot_arm_assem_v5')
+    urdf_path = os.path.join(pkg_share, 'urdf', 'robot_arm_assem_v5.urdf')
+    rviz_config_path = os.path.join(pkg_share, 'config', 'urdf.rviz')
+
+    with open(urdf_path, 'r') as f:
+        robot_description = f.read()
+
+    return LaunchDescription([
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}],
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_path],
+        ),
+    ])
